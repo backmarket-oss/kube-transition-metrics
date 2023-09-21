725036fc7499aea0db0fa04cc1c492e02aee4df4
Isabelle Erin COWAN-BERGMAN <izzi@izzette.com>
2023-09-21 13:53:43 +0200

    Fix kube client init for in-cluster

--- cmd/kube-transition-metrics/main.go
+++ cmd/kube-transition-metrics/main.go
@@ -14,8 +14,51 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 )
+
+func getKubeconfigInCluster() *rest.Config {
+	if _, present := os.LookupEnv("KUBERNETES_SERVICE_HOST"); !present {
+		return nil
+	}
+
+	// We're inside a pod, use in-cluster configuration
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		log.Panic().Err(err).Msg("Failed to get in-cluster Kubernetes config")
+	}
+
+	return config
+}
+
+func getKubeconfigFromPath(options *options.Options) *rest.Config {
+	var kubeconfigPath string
+	if options.KubeconfigPath != "" {
+		kubeconfigPath = options.KubeconfigPath
+	} else if value, present := os.LookupEnv("KUBECONFIG"); present {
+		kubeconfigPath = value
+	} else {
+		kubeconfigPath = os.Getenv("HOME") + "/.kube/config"
+	}
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	if err != nil {
+		log.Panic().Err(err).
+			Msgf("Failed to build Kubernetes config from %s", kubeconfigPath)
+	}
+
+	return config
+}
+
+func getKubeconfig(options *options.Options) *rest.Config {
+	if options.KubeconfigPath == "" {
+		if config := getKubeconfigInCluster(); config != nil {
+			return config
+		}
+	}
+
+	return getKubeconfigFromPath(options)
+}
 
 func main() {
 	prommetrics.Register()
@@ -23,15 +66,11 @@ func main() {
 	options := options.Parse()
 	zerolog.SetGlobalLevel(options.LogLevel)
 
-	kubeconfigPath := os.Getenv("HOME") + "/.kube/config"
-	if options.KubeconfigPath != "" {
-		kubeconfigPath = options.KubeconfigPath
-	} else if value, present := os.LookupEnv("KUBECONFIG"); present {
-		kubeconfigPath = value
+	config := getKubeconfig(options)
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Panic().Err(err).Msg("Failed to build kubernetes client")
 	}
-	config, _ := clientcmd.
-		BuildConfigFromFlags("", kubeconfigPath)
-	clientset, _ := kubernetes.NewForConfig(config)
 
 	initialSyncBlacklist, resourceVersion, err :=
 		statistics.CollectInitialPods(options, clientset)
